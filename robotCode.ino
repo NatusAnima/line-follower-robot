4f1830bb9e3854dd7351d7a243a55bf6bc0baae9
NatusAnima <86946056+NatusAnima@users.noreply.github.com>
2023-05-04 12:21:43 +0300

    Change declaration for sensors
    improve line following

--- robotCode.ino
+++ robotCode.ino
@@ -1,10 +1,7 @@
 #include <QTRSensors.h>
+ 
 
-#define NUM_SENSORS   8  // number of sensors used
-#define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
-#define EMITTER_PIN   2  // emitter is controlled by digital pin 2
-
-// the pins for each sensor
+ // the pins for each sensor
 #define SENSOR1_PIN A0
 #define SENSOR2_PIN A1
 #define SENSOR3_PIN A2
@@ -14,60 +11,90 @@
 #define SENSOR7_PIN A6
 #define SENSOR8_PIN A7
 
-// create an instance of the QTRSensor object
-QTRSensorsRC qtrrc((unsigned char[]) {SENSOR1_PIN, SENSOR2_PIN, SENSOR3_PIN, SENSOR4_PIN, SENSOR5_PIN, SENSOR6_PIN, SENSOR7_PIN, SENSOR8_PIN}, 
-                   NUM_SENSORS, TIMEOUT, EMITTER_PIN);
 
+#include <QTRSensors.h>
+
+#define NUM_SENSORS   8  // number of sensors used
+
+// create an instance of the QTRSensor object
+QTRSensorsRC qtrA((char[]) {SENSOR1_PIN, SENSOR2_PIN, SENSOR3_PIN, SENSOR4_PIN, SENSOR5_PIN, SENSOR6_PIN, SENSOR7_PIN, SENSOR8_PIN}, 
+                   NUM_SENSORS);
+ 
 void setup()
 {
-  // initialize the serial communication
-  Serial.begin(9600);
-  
-  // calibrate the sensors
-  qtrrc.calibrate();
-  
-  // set the motor pins as output
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  // optional: wait for some input from the user, such as  a button press
+ 
+  // then start calibration phase and move the sensors over both
+  // reflectance extremes they will encounter in your application:
+  for (int i = 0; i < 250; i++)  // make the calibration take about 5 seconds
+  {
+    qtrA.calibrate();
+    delay(20);
+  }
 }
 
 void loop()
 {
-  // read the sensor values
-  unsigned int sensorValues[NUM_SENSORS];
-  qtrrc.read(sensorValues);
-  
-  // compute the line position
-  unsigned int position = qtrrc.readLine(sensorValues);
-  
-  // print the sensor values and line position
-  Serial.print("Sensor values:");
-  for (int i = 0; i < NUM_SENSORS; i++)
+  unsigned int sensors[3];
+  // get calibrated sensor values returned in the sensors array, along with the line
+  // position, which will range from 0 to 2000, with 1000 corresponding to the line
+  // over the middle sensor.
+  int position = qtrA.readLine(sensors);
+//This method technically remembers when it last saw the black line, so the value stored in it will stay consistent
+//if we lose the black line
+
+ int leftMotorSpeed = 100;
+  int rightMotorSpeed = 100;
+  // if all eight sensors see very low reflectance, take some appropriate action for this 
+  // situation.
+  if (sensors[0] > 750 && sensors[1] > 750 && sensors[2] > 750 && sensors[3] > 750 && sensors[4] > 750 && sensors[5] > 750 && sensors[6] > 750 && sensors[7] > 750)
   {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
+    // keep pushing forward soldier.
+    leftMotorSpeed = 100;
+    rightMotorSpeed = 100;
   }
-  Serial.print("Line position:");
-  Serial.print(position);
-  Serial.println();
-  
-  // adjust the motors based on the line position
-  if (position < 3000)
-  {
-    // turn left
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
-  }
-  else if (position > 4000)
-  {
-    // turn right
-    digitalWrite(9, LOW);
-    digitalWrite(10, HIGH);
-  }
-  else
-  {
-    // go straight
-    digitalWrite(9, HIGH);
-    digitalWrite(10, HIGH);
-  }
+ 
+  // compute our "error" from the line position.  We will make it so that the error is
+  // zero when the middle sensors are over the line, because this is our goal.  Error
+  // will range from -3000 to +3000.  If we have sensor 0 on the far left and sensor 7 on
+  // the far right,  a reading of -3000 means that we see the line on the very far left and a reading
+  // of +3000 means we see the line on the very far right.
+  // zero middle sensors are sensor 4 and sensor 5
+  int error = position - 3000;
+ 
+ //The error range and the motor speed values might need to be adjusted(!)
+ if (error < -2500) {  // the line is on the far left
+  leftMotorSpeed = 0;
+  rightMotorSpeed = 255;
+} else if (error < -2000) {  // the line is on the left
+  leftMotorSpeed = 50;
+  rightMotorSpeed = 255;
+} else if (error < -1500) {  // the line is slightly on the left
+  leftMotorSpeed = 100;
+  rightMotorSpeed = 255;
+} else if (error < -1000) {  // the line is slightly on the left
+  leftMotorSpeed = 150;
+  rightMotorSpeed = 255;
+} else if (error < -500) {  // the line is on the left
+  leftMotorSpeed = 200;
+  rightMotorSpeed = 255;
+} else if (error > 2500) {  // the line is on the far right
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 0;
+} else if (error > 2000) {  // the line is on the right
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 50;
+} else if (error > 1500) {  // the line is slightly on the right
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 100;
+} else if (error > 1000) {  // the line is slightly on the right
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 150;
+} else if (error > 500) {  // the line is on the right
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 200;
+} else {  // the robot is on the line
+  leftMotorSpeed = 255;
+  rightMotorSpeed = 255;
+}
 }
