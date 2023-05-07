e368b9b9c6298c265c5995c3a647aa3d2c05d5aa
NatusAnima <86946056+NatusAnima@users.noreply.github.com>
2023-05-07 20:02:06 +0300

    Fix the main declaration
    Due to using an outdated library that was provided, the entire main section had to be rewritten

--- robotCode.ino
+++ robotCode.ino
@@ -11,13 +11,50 @@
 #define SENSOR7_PIN A6
 #define SENSOR8_PIN A7
 
-#define NUM_SENSORS 8  // number of sensors used
+QTRSensors qtr;
 
-// create an instance of the QTRSensor object
-QTRSensorsRC qtrA((unsigned char[]) {SENSOR1_PIN, SENSOR2_PIN, SENSOR3_PIN, SENSOR4_PIN, SENSOR5_PIN, SENSOR6_PIN, SENSOR7_PIN, SENSOR8_PIN}, NUM_SENSORS);
- 
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
 void setup()
 {
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){SENSOR1_PIN, SENSOR2_PIN, SENSOR3_PIN, SENSOR4_PIN, SENSOR5_PIN, SENSOR6_PIN, SENSOR7_PIN, SENSOR8_PIN}, SensorCount);
+  qtr.setEmitterPin(2);
+
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+  // analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 8 sensors
+  // * 10 reads per calibrate() call = ~32 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 13 seconds.
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+  // print the calibration minimum values measured when emitters were on
+  Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
+
   //Setup Channel A
   pinMode(12, OUTPUT); //Initiates Motor Channel A pin
   pinMode(9, OUTPUT); //Initiates Brake Channel A pin
@@ -27,79 +64,65 @@ void setup()
   pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
 
 
-  // then start calibration phase and move the sensors over both
-  // reflectance extremes they will encounter in your application:
-  for (int i = 0; i < 250; i++)  // make the calibration take about 5 seconds
-  {
-    qtrA.calibrate();
-    delay(20);
-  }
+
 }
 
 void loop()
 {
-  unsigned int sensors[3];
-  // get calibrated sensor values returned in the sensors array, along with the line
-  // position, which will range from 0 to 2000, with 1000 corresponding to the line
-  // over the middle sensor.
-  int position = qtrA.readLine(sensors);
-//This method technically remembers when it last saw the black line, so the value stored in it will stay consistent
-//if we lose the black line
 
- int leftMotorSpeed = 100;
-  int rightMotorSpeed = 100;
-  // if all eight sensors see very low reflectance, take some appropriate action for this 
-  // situation.
-  if (sensors[0] > 750 && sensors[1] > 750 && sensors[2] > 750 && sensors[3] > 750 && sensors[4] > 750 && sensors[5] > 750 && sensors[6] > 750 && sensors[7] > 750)
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 8000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
+  //This method technically remembers when it last saw the black line, so the value stored in it will stay consistent
+  //if we lose the black line
+
+  for (uint8_t i = 0; i < SensorCount; i++)
   {
-    // keep pushing forward soldier.
-    leftMotorSpeed = 100;
-    rightMotorSpeed = 100;
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
   }
+  Serial.println(position);
+
+  int leftMotorSpeed = 255;
+  int rightMotorSpeed = 255;
  
   // compute our "error" from the line position.  We will make it so that the error is
   // zero when the middle sensors are over the line, because this is our goal.  Error
-  // will range from -3000 to +3000.  If we have sensor 0 on the far left and sensor 7 on
-  // the far right,  a reading of -3000 means that we see the line on the very far left and a reading
-  // of +3000 means we see the line on the very far right.
-  // zero middle sensors are sensor 4 and sensor 5
-  int error = position - 3000;
- 
- //The error range and the motor speed values might need to be adjusted(!)
- if (error < -2500) {  // the line is on the far left
-  leftMotorSpeed = 0;
-  rightMotorSpeed = 255;
-} else if (error < -2000) {  // the line is on the left
-  leftMotorSpeed = 50;
-  rightMotorSpeed = 255;
-} else if (error < -1500) {  // the line is slightly on the left
-  leftMotorSpeed = 100;
-  rightMotorSpeed = 255;
-} else if (error < -1000) {  // the line is slightly on the left
-  leftMotorSpeed = 150;
-  rightMotorSpeed = 255;
-} else if (error < -500) {  // the line is on the left
-  leftMotorSpeed = 200;
-  rightMotorSpeed = 255;
-} else if (error > 2500) {  // the line is on the far right
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 0;
-} else if (error > 2000) {  // the line is on the right
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 50;
-} else if (error > 1500) {  // the line is slightly on the right
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 100;
-} else if (error > 1000) {  // the line is slightly on the right
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 150;
-} else if (error > 500) {  // the line is on the right
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 200;
-} else {  // the robot is on the line
-  leftMotorSpeed = 255;
-  rightMotorSpeed = 255;
-}
+  // will range from -4000 to +4000.  If we have sensor 0 on the far left and sensor 7 on
+  // the far right,  a reading of -4000 means that we see the line on the very far left and a reading
+  // of +4000 means we see the line on the very far right.
+  // zero is the sensor 4
+  int error = position - 4000;
+
+
+ if (error < -3000) {  // the line is on the far left
+    leftMotorSpeed = 0;
+    rightMotorSpeed = 255;
+  } else if (error < -2000) {  // the line is on the left
+    leftMotorSpeed = 80;
+    rightMotorSpeed = 255;
+  } else if (error < -1000) {  // the line is slightly on the left
+    leftMotorSpeed = 150;
+    rightMotorSpeed = 255;
+  } else if (error < 0) {  // the line is slightly on the left
+    leftMotorSpeed = 200;
+    rightMotorSpeed = 255;
+  } else if (error > 3000) {  // the line is on the far right
+    leftMotorSpeed = 255;
+    rightMotorSpeed = 0;
+  } else if (error > 2000) {  // the line is on the right
+    leftMotorSpeed = 255;
+    rightMotorSpeed = 80;
+  } else if (error > 1000) {  // the line is slightly on the right
+    leftMotorSpeed = 255;
+    rightMotorSpeed = 150;
+  } else if (error > 0) {  // the line is slightly on the right
+    leftMotorSpeed = 255;
+    rightMotorSpeed = 200;
+  } else {  // the robot is on the line
+    leftMotorSpeed = 255;
+    rightMotorSpeed = 255;
+  }
 
   //Motor A forward @ full speed
   digitalWrite(12, HIGH);  //Establishes forward direction of Channel A
